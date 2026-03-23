592be2366ba952570d531d7b528c2541652ed6bd
Manish Mathur <madiv@Manishs-MacBook-Pro.local>
2026-03-23 17:06:23 +0530

    feat(cpp): add initial C++ solution skeleton for Valid Palindrome 🧩

---
+++ source/cpp/solution.hpp
@@ -0,0 +1,14 @@
+#define SOLUTION_HPP
+#ifdef SOLUTION_HPP
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        return 0;
+    }
+};
+
+#endif
